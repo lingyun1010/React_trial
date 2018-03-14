579f2d1c33de7d312bcd4bbdc4ab6c5a2d7dea0e
LingyunZhao <zhaolingyun1010@gmail.com>
2018-03-14 08:42:29 +0000

    full version

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -6,17 +6,12 @@ class SearchBar extends Component {
 
 		super(props);
 
-<<<<<<< HEAD
 		this.state = {term: ''};
-=======
-		this.state = {term: 'search'};
->>>>>>> c6dba14aa6ac18b9c04ee61520d14f26ab905909
 	}
 
 	render(){
 
 		return (
-<<<<<<< HEAD
 			<div className="search-bar">
 				<input 
 				value = {this.state.term}
@@ -30,15 +25,6 @@ class SearchBar extends Component {
 		this.props.onSearchTermChange(term);
 
 	}
-=======
-			<div>
-				<input 
-				value = {this.state.term}
-				onChange={event => this.setState({term: event.target.value})}/>
-			</div>
-		);
-	}
->>>>>>> c6dba14aa6ac18b9c04ee61520d14f26ab905909
 }
 
 export default SearchBar;
